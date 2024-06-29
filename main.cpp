99d42303be082f16339e54c3ee82b7b922e6a533
magnusdice <joblicious13@gmail.com>
2024-06-29 15:34:35 +0800

    added random stuff and change a bit stuff and yes

--- main.cpp
+++ main.cpp
@@ -1,19 +1,36 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Hello Git, I use arch btw";
+    std::cout << "Hello Git, I use arch btw" <<std::endl;
 
-    std::cout << "This is just a test and learning how to do git things"<<endl;
-    std::cout << "git add * - adds the all the files" <<endl;
-    std::cout << "git add <file> - for specifics" << endl;
-    std::cout << "git remove  * - remove all" << endl;
-    std::cout << "git remove <file> - remove for specifics" << endl;
+    std::cout << "This is just a test and learning how to do git things"<< std::endl;
+    std::cout << "git add * - adds the all the files" <<std::endl;
+    std::cout << "git add <file> - for specifics" << std::endl;
+    std::cout << "git remove  * - remove all" << std::endl;
+    std::cout << "git remove <file> - remove for specifics" << std::endl;
+    std::cout <<"\n";
 
-    std:: cout << "git commit -m <changes note> - to commit the modified code" <<endl;
-    std:: cout << "git push - to push all the file that has been commited" << endl;
+    std:: cout << "git commit -m <changes note> - to commit the modified code" <<std::endl;
+    std:: cout << "git push - to push all the file that has been commited" << std::endl;
+    std::cout <<"\n";
 
 
-    std::cout  <<" Hello future me, how to compile this stuff? just g++ main.cpp -o <filename>"
+    std::cout  <<" Hello future me, how to compile this stuff? just g++ main.cpp -o <filename>" <<std::endl;
+    std::cout <<"\n";
+    std::cout <<"\n";
+    std::cout <<"\n";
+    std::cout <<"\n";
+    // add some random shit
+
+    for (unsigned long long i = 1; i < 6008514751437ULL; i++){
+            std::cout << "YES" <<std::endl; 
+        
+        for (unsigned long long y=1 ; y < 600851475143ULL; y++){
+            std::cout << "YES" <<std::endl; 
+        }
+    }
+
+
 
     return 0;
 }
