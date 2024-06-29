f4c6fcd2d7445a1ebb0128e3cf2810ee5249840d
magnusdice <joblicious13@gmail.com>
2024-06-29 15:17:11 +0800

    Added git commit and push

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Hello Git";
+    std::cout << "Hello Git, I use arch btw";
+
+    std::cout << "This is just a test and learning how to do git things"<<endl;
+    std::cout << "git add * - adds the all the files" <<endl;
+    std::cout << "git add <file> - for specifics" << endl;
+    std::cout << "git remove  * - remove all" << endl;
+    std::cout << "git remove <file> - remove for specifics" << endl;
+
+    std:: cout << "git commit -m <changes note> - to commit the modified code" <<endl;
+    std:: cout << "git push - to push all the file that has been commited" << endl;
+
+
+    std::cout  <<" Hello future me, how to compile this stuff? just g++ main.cpp -o <filename>"
+
     return 0;
 }
